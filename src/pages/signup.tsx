5f945d757363f09348025b5238e091df0eea86f0
James Kim <james.kim.nz92@gmail.com>
2021-04-02 17:09:48 +1300

    basic client-side validation, with react-hooks-form

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from '@material-ui/core/Button';
+import { useForm } from "react-hook-form";
+
+// from https://emailregex.com/
+const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
 const useStyles = makeStyles((theme) => ({
   form: {
@@ -14,15 +18,21 @@ const useStyles = makeStyles((theme) => ({
 
 export const SignUpPage = () => {
   const classes = useStyles();
+  const { register, handleSubmit, watch, errors } = useForm();
+  const password = watch("password");
+
   const onSubmit = () => {
     alert(`signed up!`) 
   };
 
   return (
-    <form className={classes.form} onSubmit={onSubmit}>
+    <form className={classes.form} onSubmit={e => e.preventDefault()}>
       <div>
         <TextField
-          id="email-input"
+          name="email"
+          inputRef={register({ required: "You must provide an email", pattern: emailRegex })}
+          error={errors.email}
+          helperText={errors?.email?.message}
           label="Email"
           type="email"
           autoComplete="email"
@@ -31,7 +41,13 @@ export const SignUpPage = () => {
       </div>
       <div>
         <TextField
-          id="password-input"
+          name="password"
+          inputRef={register({ required: "You must specify a password", minLength: {
+              value: 8,
+              message: "Password must have at least 8 characters"
+          } })}
+          error={errors.password}
+          helperText={errors?.password?.message}
           label="Password"
           type="password"
           autoComplete="new-password"
@@ -40,7 +56,10 @@ export const SignUpPage = () => {
       </div>
       <div>
         <TextField
-          id="password-confirm"
+          name="passwordConfirm"
+          inputRef={register({ validate: value => value === password || "The passwords do not match"})}
+          error={errors.passwordConfirm}
+          helperText={errors?.passwordConfirm?.message}
           label="Confirm Password"
           type="password"
           autoComplete="new-password"
@@ -48,7 +67,7 @@ export const SignUpPage = () => {
         />
       </div>
 
-        <Button type="submit">
+        <Button type="submit" onClick={handleSubmit(onSubmit)}>
             Sign up
         </Button> 
     </form>
